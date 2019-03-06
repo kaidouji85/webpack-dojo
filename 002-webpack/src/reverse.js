e6c96a42576b69d7e338b69611bffcdf3bbbdb18
竹内佑介 <kaidouji85@gmail.com>
2019-03-06 19:28:45 +0900

    webpackを導入した

---
+++ 002-webpack/src/reverse.js
@@ -0,0 +1,25 @@
+/**
+ * 文字列反転処理結果を表示する
+ *
+ * @param {string} originText 反転対象の文字
+ * @return {string} 反転結果
+ */
+export function createReverseResult(originText) {
+  if (originText === '') {
+    return '何も入力されていません。';
+  }
+
+  return `反転結果: ${reverseString(originText)}`;
+}
+
+/**
+ * 文字列を反転する
+ *
+ * @param {string} value 変換元
+ * @return {string} 変換結果
+ */
+function reverseString(value) {
+  return value.split('')
+    .reverse()
+    .join('');
+}
